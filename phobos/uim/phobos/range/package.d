55b6640da37d8282273c67109ced5e6e4c403702
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-06 14:47:52 +0200

    Add new modules for algorithm, container, datetime, digest, format, logger, math, net, and range

--- phobos/uim/phobos/range/package.d
+++ phobos/uim/phobos/range/package.d
@@ -3,30 +3,5 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.phobos;
+module uim.phobos.range;
 
-
-/* mixin(ImportPhobos!());
-public import vibe.d; */
-
-public {
-  import std.stdio;
-  import std.string;
-/*   import uim.phobos.classes;
-  import uim.phobos.containers;
-  import uim.phobos.convert;
-  import uim.phobos.datatypes;
- */  
- // import uim.phobos.enumerations;
-/*   import uim.phobos.helpers;
-  import uim.phobos.io;
-  import uim.phobos.mixins;
-  import uim.phobos.web;
-  import uim.phobos.tests;
- */}
-
-/* version (test_uim_core) {
-  unittest {
-    writeln("-----  ", __MODULE__, "\t  -----");
-  }
-} */
