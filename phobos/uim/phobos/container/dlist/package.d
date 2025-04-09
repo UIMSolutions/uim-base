211acaee8d8103f71a8e94e705965fba968aa206
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-09 09:59:26 +0200

    Working on supackages

--- phobos/uim/phobos/container/dlist/package.d
+++ phobos/uim/phobos/container/dlist/package.d
@@ -3,13 +3,5 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
-module uim.phobos.container;
+module uim.phobos.container.dlist;
 
-public {
-    import uim.phobos.container.array;
-    import uim.phobos.container.binaryheap;
-    import uim.phobos.container.dlist;
-    import uim.phobos.container.rbtree;
-    import uim.phobos.container.slist;
-    import uim.phobos.container.util;
-}
