a766e7755b7a59c21ac576e7141133f28c532d82
UIMSolutions <ui.manufaktur@gmail.com>
2025-05-18 15:04:38 +0200

    Refactor error interfaces; remove unused error-related interfaces and add new error node and renderer interfaces

--- errors/uim/errors/interfaces/package.d
+++ errors/uim/errors/interfaces/package.d
@@ -9,3 +9,16 @@ public {
     import uim.errors.interfaces.errors;
     import uim.errors.interfaces.exceptions;
 }
+
+public {
+    import uim.errors.interfaces.errors.error;
+
+    import uim.errors.interfaces.errors.controller;
+    import uim.errors.interfaces.errors.debugger;
+    import uim.errors.interfaces.errors.formatter;
+    import uim.errors.interfaces.errors.handler;
+    import uim.errors.interfaces.errors.logger;
+    import uim.errors.interfaces.errors.middleware;
+    import uim.errors.interfaces.errors.node;
+    import uim.errors.interfaces.errors.renderer;
+}
