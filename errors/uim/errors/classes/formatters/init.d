eb11b2727239ea8aec5f13b88a05c7a3ba56f1d0
UIMSolutions <ui.manufaktur@gmail.com>
2025-05-24 08:41:32 +0200

    Refactor init.d: add error formatter registrations for console, json, text, xml, and yaml

--- errors/uim/errors/classes/formatters/init.d
+++ errors/uim/errors/classes/formatters/init.d
@@ -5,7 +5,27 @@ import uim.errors;
 @safe:
 
 static this() {
+  ErrorFormatterFactory.set("console", (Json[string] options = null) @safe {
+    return new DConsoleErrorFormatter(options);
+  });
+
   ErrorFormatterFactory.set("html", (Json[string] options = null) @safe {
     return new DHtmlErrorFormatter(options);
+  });
+
+  ErrorFormatterFactory.set("json", (Json[string] options = null) @safe {
+    return new DJsonErrorFormatter(options);
+  });
+
+  ErrorFormatterFactory.set("text", (Json[string] options = null) @safe {
+    return new DTextErrorFormatter(options);
+  });
+
+  ErrorFormatterFactory.set("xml", (Json[string] options = null) @safe {
+    return new DXmlErrorFormatter(options);
+  });
+
+  ErrorFormatterFactory.set("yaml", (Json[string] options = null) @safe {
+    return new DYamlErrorFormatter(options);
   });
 }
