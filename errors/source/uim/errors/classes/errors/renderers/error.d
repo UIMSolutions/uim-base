308d1404d1ac2e007acf3050f15050c6dfef3c3d
UIMSolutions <ui.manufaktur@gmail.com>
2025-10-10 08:28:06 +0200

    Update license comments across multiple files to ensure consistency in the Apache 2.0 license statement. Modified files include various JSON data types, enumerations, helpers, mixins, and package definitions in the Vibe and YAML modules.

--- errors/source/uim/errors/classes/errors/renderers/error.d
+++ errors/source/uim/errors/classes/errors/renderers/error.d
@@ -1,6 +1,6 @@
 /****************************************************************************************************************
 * Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
-* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 *****************************************************************************************************************/
 module uim.errors.classes.errors.renderers.error;
@@ -360,7 +360,7 @@ class UIMErrorRenderer : UIMObject, IErrorRenderer {
 /* Old code
 /****************************************************************************************************************
 * Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
-* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
 ***************************************************************************************************************** /
 module uim.errors.classes.errors.renderer;
