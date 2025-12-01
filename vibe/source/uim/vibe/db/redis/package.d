a55c17d1aed071b073eb1aaf2ef246442410f551
UIMSolutions <ui.manufaktur@gmail.com>
2025-12-01 16:15:33 +0100

    Add new Redis and HTTP modules; include idioms, session store, and various HTTP components

--- vibe/source/uim/vibe/db/redis/package.d
+++ vibe/source/uim/vibe/db/redis/package.d
@@ -1,2 +1,8 @@
 module uim.vibe.db.redis;
 
+public {
+  import uim.vibe.db.redis.types;
+  import uim.vibe.db.redis.sessionstore;
+  import uim.vibe.db.redis.idioms;
+  import uim.vibe.db.redis.redis;
+}
