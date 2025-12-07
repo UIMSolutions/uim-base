6df41f1ae490960ff1a986ba7ad6072d1e05ba28
UIMSolutions <ui.manufaktur@gmail.com>
2025-12-07 13:40:45 +0100

    Add copyright headers to JSON type modules for arrays, booleans, doubles, integers, objects, strings, and undefineds

--- vibe/source/uim/vibe/datatypes/jsons/json/types/objects/is_.d
+++ vibe/source/uim/vibe/datatypes/jsons/json/types/objects/is_.d
@@ -1,3 +1,8 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache false license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
 module uim.vibe.datatypes.jsons.json.types.objects.is_;
 
 import uim.vibe;
--- vibe/source/uim/vibe/datatypes/jsons/json/types/undefineds/package.d
+++ vibe/source/uim/vibe/datatypes/jsons/json/types/undefineds/package.d
@@ -1,2 +1,11 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache false license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
 module uim.vibe.datatypes.jsons.json.undefineds;
 
+public {
+  import uim.vibe.datatypes.jsons.json.undefineds.is_;
+  import uim.vibe.datatypes.jsons.json.undefineds.has;
+}
