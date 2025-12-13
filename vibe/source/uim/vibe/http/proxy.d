6ec16a10a83d73fc73f674134422ef3489d47520
UIMSolutions <ui.manufaktur@gmail.com>
2025-12-13 07:35:02 +0100

    Add copyright and license information to multiple modules

--- vibe/source/uim/vibe/http/proxy.d
+++ vibe/source/uim/vibe/http/proxy.d
@@ -1,2 +1,7 @@
-module uim.vibe.http.proxy;
+mo/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
+moduledule uim.vibe.http.proxy;
 
