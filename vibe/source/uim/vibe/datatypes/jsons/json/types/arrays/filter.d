5edd45b5e3be0339bbfab1a0b88c7126c9706e10
UIMSolutions <ui.manufaktur@gmail.com>
2025-12-19 15:03:27 +0100

    Add filtering, first, last, and remove functions for booleans in JSON arrays; update package imports and add copyright information.

---
+++ vibe/source/uim/vibe/datatypes/jsons/json/types/arrays/filter.d
@@ -0,0 +1,24 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
+module uim.vibe.datatypes.jsons.json.types.arrays.filter;
+
+import uim.vibe;
+
+mixin(Version!("test_uim_vibe"));
+
+@safe:
+
+Json[] filterBooleans(Json[] jsons, bool delegate(Json json) @safe filterFunc) {
+  return jsons.filterBooleans.filter!(json => filterFunc(json)).array;
+}
+
+Json[] filterBooleans(Json[] jsons, size_t[] indices) {
+  return jsons.filterIndices(indices).filterBooleans.array;
+}
+
+Json[] filterBooleans(Json[] jsons) {
+  return jsons.filter!(json => json.isBoolean).array;
+}
