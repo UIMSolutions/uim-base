4d44108d582e63348efb0a66703216029b7824bb
UIMSolutions <ui.manufaktur@gmail.com>
2026-01-13 16:27:37 +0100

    feat(yaml): Introduce YAML module with parsing, creation, and validation features
    - Added new `yaml` subpackage to handle YAML data types and operations.
    - Implemented functions for checking YAML node types (scalar, sequence, mapping, null).
    - Created utilities for converting YAML nodes to strings and JSON.
    - Developed functions to create YAML nodes from key-value pairs, associative arrays, and arrays.
    - Added parsing capabilities for YAML strings and files, including support for multiple documents.
    - Implemented functions to retrieve values from YAML nodes by key and path, with default value options.
    - Included example usage in `examples/yaml_example.d` to demonstrate the new functionality.
    - Updated `dub.sdl` and `dub.selections.json` to include the new `yaml` dependency.

--- yaml/source/uim/yaml/datatypes/yamls/package.d
+++ yaml/source/uim/yaml/datatypes/yamls/package.d
@@ -3,17 +3,12 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)
 *****************************************************************************************************************/
-module uim.core.datatypes;
+module uim.core.datatypes.yamls;
 
 public {
-  import uim.core.datatypes.booleans;
-  import uim.core.datatypes.datetimes;
-  import uim.core.datatypes.jsons;
-  import uim.core.datatypes.yamls;
-  import uim.core.datatypes.bsons;
-  import uim.core.datatypes.longs;
-  import uim.core.datatypes.mixins;
-  import uim.core.datatypes.nulls;
-  import uim.core.datatypes.strings;
-  import uim.core.datatypes.uuids;
+  import uim.core.datatypes.yamls.check;
+  import uim.core.datatypes.yamls.convert;
+  import uim.core.datatypes.yamls.create;
+  import uim.core.datatypes.yamls.parse;
+  import uim.core.datatypes.yamls.values;
 }
---
+++ yaml/source/uim/yaml/datatypes/yamls/values.d
@@ -0,0 +1,130 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
+module uim.yaml.datatypes.yamls.values;
+
+import uim.yaml;
+import dyaml;
+import std.conv;
+
+mixin(ShowModule!());
+
+@safe:
+
+/**
+ * Get a value from a YAML Node by key
+ * Params:
+ *   node = The YAML Node to query
+ *   key = The key to retrieve
+ * Returns: The value at the specified key
+ */
+Node getYamlValue(Node node, string key) {
+  if (node.isMapping && node.containsKey(key)) {
+    return node[key];
+  }
+  return Node.init;
+}
+
+/**
+ * Get a value from a YAML Node by key with a default value
+ * Params:
+ *   node = The YAML Node to query
+ *   key = The key to retrieve
+ *   defaultValue = The default value to return if key doesn't exist
+ * Returns: The value at the specified key or the default value
+ */
+T getYamlValueOr(T)(Node node, string key, T defaultValue) {
+  if (node.isMapping && node.containsKey(key)) {
+    auto value = node[key];
+    if (value.convertsTo!T) {
+      return value.as!T;
+    }
+  }
+  return defaultValue;
+}
+
+/**
+ * Get a value from a YAML sequence by index
+ * Params:
+ *   node = The YAML sequence Node
+ *   index = The index to retrieve
+ * Returns: The value at the specified index
+ */
+Node getYamlValueAt(Node node, size_t index) {
+  if (node.isSequence && index < node.length) {
+    return node[index];
+  }
+  return Node.init;
+}
+
+/**
+ * Get all keys from a YAML mapping Node
+ * Params:
+ *   node = The YAML mapping Node
+ * Returns: Array of all keys
+ */
+string[] getYamlKeys(Node node) {
+  string[] keys;
+  
+  if (node.isMapping) {
+    foreach (string key, Node value; node) {
+      keys ~= key;
+    }
+  }
+  
+  return keys;
+}
+
+/**
+ * Get all values from a YAML mapping Node
+ * Params:
+ *   node = The YAML mapping Node
+ * Returns: Array of all values
+ */
+Node[] getYamlValues(Node node) {
+  Node[] values;
+  
+  if (node.isMapping) {
+    foreach (string key, Node value; node) {
+      values ~= value;
+    }
+  }
+  
+  return values;
+}
+
+/**
+ * Get the length/size of a YAML Node
+ * Params:
+ *   node = The YAML Node
+ * Returns: The number of elements in the node
+ */
+size_t getYamlLength(Node node) {
+  if (node.isSequence || node.isMapping) {
+    return node.length;
+  }
+  return 0;
+}
+
+/**
+ * Get a nested value from a YAML Node using a path
+ * Params:
+ *   node = The YAML Node to query
+ *   path = Array of keys representing the path to the value
+ * Returns: The value at the specified path
+ */
+Node getYamlValueByPath(Node node, string[] path) {
+  Node current = node;
+  
+  foreach (key; path) {
+    if (current.isMapping && current.containsKey(key)) {
+      current = current[key];
+    } else {
+      return Node.init;
+    }
+  }
+  
+  return current;
+}
