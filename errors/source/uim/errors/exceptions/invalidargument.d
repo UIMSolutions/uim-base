66f6248797e772603cc9d879369a37f38a929e5c
UIMSolutions <ui.manufaktur@gmail.com>
2026-01-18 13:00:59 +0100

    Add error handling framework with enumerations, exceptions, factories, and interfaces
    - Introduced error enumerations in `uim.errors.enumerations.errors` to categorize different error types.
    - Created custom exceptions: `DInvalidArgumentException` and `DUnknownEditorException` in `uim.errors.exceptions`.
    - Implemented error factories for creating error instances and formatting errors in `uim.errors.factories`.
    - Established interfaces for error handling, including controllers, debuggers, formatters, and renderers in `uim.errors.interfaces`.
    - Developed mixins for error handling functionalities, allowing for reusable error handling logic across different components.
    - Added comprehensive package structure to organize the error handling modules effectively.

---
+++ errors/source/uim/errors/exceptions/invalidargument.d
@@ -0,0 +1,32 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
+module uim.errors.exceptions.invalidargument;
+
+import uim.errors;
+mixin(Version!("test_uim_errors"));
+
+@safe:
+
+class DInvalidArgumentException : Exception {
+  this(string message, string fileName = null, ulong lineNumber = 0, Throwable nextInChain = null) {
+    super(message, fileName, lineNumber, nextInChain);
+  }
+}
+
+auto InvalidArgumentException() {
+  return new DInvalidArgumentException("Invalid Argumnent");
+}
+
+auto InvalidArgumentException(string message) {
+  return new DInvalidArgumentException(message);
+}
+
+unittest {
+  auto exception = new InvalidArgumentException();
+  assert(exception !is null, "Failed to create InvalidArgumentException instance");
+
+  assert(testException(exception), "Test for InvalidArgumentException failed");
+}
---
+++ errors/source/uim/errors/factories/formatter.d
@@ -0,0 +1,23 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
+module uim.errors.factories.formatter;
+
+mixin(Version!"test_uim_errors");
+
+import uim.errors;
+
+@safe:
+
+class UIMErrorFormatterFactory : DFactory!UIMErrorFormatter {
+}
+
+auto ErrorFormatterFactory() {
+    return UIMErrorFormatterFactory.instance;
+}
+
+auto ErrorFormatterFactory(string key) {
+    return ErrorFormatterFactory.create(key);   
+}
