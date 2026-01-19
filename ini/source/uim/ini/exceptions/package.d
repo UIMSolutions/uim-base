bf13b0da8aed67fe2f3b0f111531165efd1c2cb5
UIMSolutions <ui.manufaktur@gmail.com>
2026-01-19 18:11:46 +0100

    feat(ini): add helpers and exceptions modules with copyright information

--- ini/source/uim/ini/exceptions/package.d
+++ ini/source/uim/ini/exceptions/package.d
@@ -3,18 +3,4 @@
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
 * Authors: Ozan Nurettin Süel (aka UIManufaktur)
 *****************************************************************************************************************/
-module uim.ini;
-
-public { // Packages
-  import uim.ini.helpers;
-  import uim.ini.exceptions;
-}
-
-public { // Modules
-  import uim.ini.section;
-  import uim.ini.property;
-  import uim.ini.document;
-  import uim.ini.parser;
-  import uim.ini.writer;
-  import uim.ini.exceptions;
-}
+module uim.ini.exceptions;
