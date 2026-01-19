a50fa1dbea70225dbf43ee9da177290c14dad360
UIMSolutions <ui.manufaktur@gmail.com>
2026-01-19 16:08:45 +0100

    Add JSON-LD library implementation for UIM framework
    - Introduced `uim-jsonld` library with necessary dependencies.
    - Implemented `DJSONLDBuilder` for fluent construction of JSON-LD documents.
    - Created `DJSONLDContext` for managing JSON-LD context definitions.
    - Developed `DJSONLDDocument` to represent complete JSON-LD documents.
    - Added exception handling with custom exceptions for JSON-LD operations.
    - Implemented `DJSONLDGraph` to manage collections of nodes.
    - Defined JSON-LD keywords in `JSONLDKeywords` for easy reference.
    - Created `DJSONLDNode` class to represent individual nodes in JSON-LD.
    - Added support for common XSD and RDF types in `XSDTypes` and `RDFTypes`.
    - Included unit tests to validate functionality of the JSON-LD components.

---
+++ jsonld/source/uim/jsonld/package.d
@@ -0,0 +1,17 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UIManufaktur) 
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+*****************************************************************************************************************/
+module uim.jsonld;
+
+public {
+  import uim.jsonld.context;
+  import uim.jsonld.document;
+  import uim.jsonld.node;
+  import uim.jsonld.graph;
+  import uim.jsonld.builder;
+  import uim.jsonld.keywords;
+  import uim.jsonld.types;
+  import uim.jsonld.exceptions;
+}
